11f3cbbfb0ade6b27f83c628a95e774c06cf4ae0
vdelaide <adelaidemt9479@gmail.com>
2023-11-10 22:45:10 -0600

    Added styles to headline, added img, created header component, created json file for article(s)

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./header.module.css";
+
+const Header = () =>{
+	return(
+		<header>
+			<p>placeholder</p>
+		</header>
+	);
+};
+
+export default Header;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,36 +1,61 @@
 import React from "react";
 import style from "./home.module.css";
 
+// import Data from "../data.json";
+import * as All from "../components/index";
+
 const Home = () =>{
 	return(
-		<main>
+		<React.Fragment>
 
-			<section id={style["headline"]}>
+			<All.Header />
 
-				<h1 id={style["title"]}>OPINION | Child Labor Laws Stifle Innovation</h1>
-				<h2>by <cite>Adelaide "Addy" Martinez-Tapia</cite></h2>
+			<main>
 
-			</section>
+				<section id={style["headline"]}>
 
-			<section id={style["body-content"]}>
+					<img 
+					src="https://rightsofchildlabor.weebly.com/uploads/2/3/7/5/23752031/5067419_orig.jpg"
+					alt="Smiling children milking cows"/>
 
-				<aside>
-					<article>
-						<a href="https://google.com"></a>
-					</article>
+					<h1 id={style["title"]}>OPINION | Child Labor Laws Stifle Innovation</h1>
+					<h2>by <cite>Adelaide "Addy" Martinez-Tapia dot <time dateTime="2023-11-10">10 Nov. 2023</time></cite></h2>
 
-					<article>
-						<a href="https://google.com"></a>
-					</article>
+				</section>
 
-					<article>
-						<a href="https://google.com"></a>
-					</article>
-				</aside>
+				<section id={style["body-content"]}>
 
-			</section>
-			
-		</main>
+					<aside id={style["side-articles"]}>
+
+						<article>
+							<a href="https://google.com">
+								placeholder
+							</a>
+						</article>
+
+						<article>
+							<a href="https://google.com">
+								placeholder
+							</a>
+						</article>
+
+						<article>
+							<a href="https://google.com">
+								placeholder
+							</a>
+						</article>
+
+					</aside>
+
+					<p id={style["body-text"]}>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+
+				</section>
+				
+			</main>
+
+		</React.Fragment>
 	);
 };
 
